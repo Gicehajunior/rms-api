d8a662884aad8445be70895a732a0b2ecd2aaa3a
Gicehajunior <gicehajunior76@gmail.com>
2024-03-12 23:11:52 +0300

    initial dev't commit

--- src/RMSAPI.php
+++ src/RMSAPI.php
@@ -14,10 +14,11 @@ class RMSAPI {
         $this->app_id = $app_id; 
     }
         
-    function zipFiles($zipFileName, $excludedFiles = []) {
-        try { 
-            $zip = new ZipArchive(); 
+    function compile_update($zipFileName, $excludedFiles = []) {
+        $zipFileName = $zipFileName . '.zip';
 
+        try { 
+            $zip = new \PhpZip\ZipFile();
             if ($zip->open($zipFileName, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) { 
                 $files = scandir('.');
                 foreach ($files as $file) { 
@@ -28,23 +29,60 @@ class RMSAPI {
                 
                 $zip->close();
                 
-                return true;
+                $output = [
+                    'success' => 'success',
+                    'message' => 'An error occurred on. Please retry again.'
+                ];
             } else {
-                return false;
+                $output = [
+                    'success' => 'error',
+                    'message' => 'An error occurred on. Please retry again!'
+                ];
             }
         } catch (\Throwable $th) {
-            return false;
+            $output = [
+                'success' => 'error',
+                'message' => ' ' . $th
+            ];
         }
-    }
-
-    public function check_updates() {
         
+        return $output;
     }
 
-    public function update_rms() {
-        if (!empty($this->api_key) && !empty($this->app_id)) {
+    public function check_updates() { 
+        $output = [
+            'status' => 'success',
+            'message' => 'Updates available. To download, click download updates button.', 
+        ];
+
+        return $output;
+    }
+
+    public function download_updates() {
+        if (!empty($this->api_key) && !empty($this->app_id)) { 
+            $ch = curl_init();
+            $url = 'https://rms-v1.daphascomputerconsultants.com/api/rms/download-updates';
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+            $response = curl_exec($ch);
             
+            if(curl_errno($ch)){
+                $output = 'Curl error: ' . curl_error($ch);
+            } else { 
+                $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                if ($httpCode == 200) { 
+                    $output = $response;
+                } else { 
+                    $output =  'HTTP Error: ' . $httpCode;
+                }
+            }
+            
+            curl_close($ch);
+            
+            return $output;
         }
+
+        return false;
     }
 
     public function invoice ($invoicing_endpoint = null) {  
